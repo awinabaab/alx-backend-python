0b66a22ce9488f2296dbc96cb5d3ea82a3c900a9
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2025-04-24 12:31:16 +0000

    feat(0x00-python_variable_annotations): add 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""A type annotated function that cocatenates two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings
+
+    Parameters:
+        str1 (str): first string
+        str2 (str): second string
+
+    Returns:
+        Concatenated @str1 and @str2
+    """
+
+    return str1 + str2
