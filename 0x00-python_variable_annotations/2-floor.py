2480b0511db4f7d548944c80d5d30326335a39bc
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2025-04-24 12:33:28 +0000

    feat(0x00-python_variable_annotations): add 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""A type-annotated function that floors a float"""
+
+
+def floor(n: float) -> int:
+    """
+    Floors a float
+
+    Parameters:
+        n (float): number to floor
+
+    Return:
+        floored value of @n
+    """
+    return int(n)
