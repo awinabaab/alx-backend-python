76fae0236746040647ed311060fb22d8c4726c06
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2025-04-24 12:29:42 +0000

    feat(0x00-python_variable_annotations): add 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""A type annotated function that adds two floats"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating point numbers
+
+    Parameters:
+        a (float): first number
+        b (float): second number
+
+    Returns:
+        Sum of @a and @b
+    """
+
+    return a + b
