6a1c51b325860b87675e602129ac8d990d89daa6
Abdul-Mumin Awinaba <awinabaab@gmail.com>
2025-04-24 12:58:15 +0000

    feat(0x00-python_variable_annotations): add 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""A type-annotated function that converts a float to a string"""
+
+
+def to_str(n: float) -> str:
+    """
+    Produces the string representation of a float
+
+    Paramters:
+        n (float): float to convert
+
+    Returns:
+        String representation of @n
+    """
+    return str(n)
